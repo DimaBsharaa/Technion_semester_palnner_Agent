5890b10a174036e95ae3cf98832d1602aa7dfc00
Manhal Ghoummaid <manhalgh99@gmail.com>
2026-07-22 12:40:10 +0300

    team_info: team name APEX, student order Manhal, Dima, Diyar

--- agent/main.py
+++ agent/main.py
@@ -230,11 +230,11 @@ def root_gui():
 def team_info():
     return {
         "group_batch_order_number": os.environ.get("GROUP_BATCH_ORDER", "TODO_batch_order"),
-        "team_name": os.environ.get("TEAM_NAME", "Semester Planning Agent"),
+        "team_name": os.environ.get("TEAM_NAME", "APEX"),
         "students": [
             {"name": "Manhal Ghoummaid", "email": "manhal@campus.technion.ac.il"},
-            {"name": "Diyar Husayyan", "email": "diyarh@campus.technion.ac.il"},
             {"name": "Dima Bshara", "email": "dima.bshara@campus.technion.ac.il"},
+            {"name": "Diyar Husayyan", "email": "diyarh@campus.technion.ac.il"},
         ],
     }
 
